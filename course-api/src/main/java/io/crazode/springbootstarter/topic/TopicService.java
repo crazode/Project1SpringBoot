b22bfe1ba19d2cdab2814fbcfa503d459f5439b9
Iram Fatima <iram.kv5@gmail.com>
2020-10-21 17:33:07 +0530

    Business Service addition

---
+++ course-api/src/main/java/io/crazode/springbootstarter/topic/TopicService.java
@@ -0,0 +1,24 @@
+package io.crazode.springbootstarter.topic;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	private List<Topic> topics = Arrays.asList(
+			new Topic("spring", "Spring framework", "Spring framework description"),
+			new Topic("java", "Core java", "Core java description"),
+			new Topic("javascript", "Javascript", "Javascript description")
+			);
+	
+	public List<Topic> getAllTopics(){
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+}
