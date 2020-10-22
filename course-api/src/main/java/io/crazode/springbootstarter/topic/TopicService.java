35193162c3d107dc3fef67cb3e63fae0f13b2930
Iram Fatima <iram.kv5@gmail.com>
2020-10-22 17:23:13 +0530

    without database

--- course-api/src/main/java/io/crazode/springbootstarter/topic/TopicService.java
+++ course-api/src/main/java/io/crazode/springbootstarter/topic/TopicService.java
@@ -1,5 +1,6 @@
 package io.crazode.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 	
-	private List<Topic> topics = Arrays.asList(
+	private List<Topic> topics = new ArrayList<Topic>(Arrays.asList(
 			new Topic("spring", "Spring framework", "Spring framework description"),
 			new Topic("java", "Core java", "Core java description"),
 			new Topic("javascript", "Javascript", "Javascript description")
-			);
+			));
 	
 	public List<Topic> getAllTopics(){
 		return topics;
@@ -20,5 +21,22 @@ public class TopicService {
 	
 	public Topic getTopic(String id) {
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+	
+	public void updateTopic(String id, Topic topic) {
+		for(int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if(t.getId().equals(id)){
+				topics.set(i, topic);
+			}
+		}
+	}
+	
+	public void deleteTopic(String id) {
+		topics.removeIf(t -> t.getId().equals(id));
 	}
 }
